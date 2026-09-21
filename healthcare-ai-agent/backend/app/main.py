4844b8087dde24d1390e58e891a0b166eb5d2700
Pechetti lilly lakshmi puja sri <s210417@rguktsklm.ac.in>
2026-09-21 19:45:04 +0530

    Update main.py

--- healthcare-ai-agent/backend/app/main.py
+++ healthcare-ai-agent/backend/app/main.py
@@ -24,7 +24,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins==[
+    allow_origins=[
         "https://healthcare-ai-voice-agent-two.vercel.app",
     ],
     allow_credentials=True,
